18c7d79547afc800c9766ee046c0a075daef4308
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-01 01:41:13 -0500

    reply schema/routes/front-end functionality

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,19 +1,28 @@
 const router = require('express').Router();
 const {
   addComment,
-  removeComment    
+  removeComment,
+  addReply,
+  removeReply
 } = require('../../controllers/comment-controller');
 
 // POST route for comments
 router
   .route('/:pizzaId')
-  .post(addComment)
+  .post(addComment);
 
 
-// DELETE route for comments
+// PUT/DELETE route for comments
 router
   .route('/:pizzaId/:commentId')
+  .put(addReply)
   .delete(removeComment);
+
+// DELETE route for replies
+router
+  .route('/:pizzaId/:commentId/:replyId')
+  .delete(removeReply);
+
 
 
 module.exports = router;
